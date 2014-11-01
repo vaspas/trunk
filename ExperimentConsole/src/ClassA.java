a0ad5ced1dddff57e659040348dda520b3f4d6cf
vaspas <vaspas@mail.ru>
2014-11-01 20:21:00 +0400

    ...

---
+++ ExperimentConsole/src/ClassA.java
@@ -0,0 +1,5 @@
+
+public class ClassA {
+public int F1;
+public int F2;
+}
--- ExperimentConsole/src/ExperimentConsole.java
+++ ExperimentConsole/src/ExperimentConsole.java
@@ -10,6 +10,12 @@ public class ExperimentConsole {
 		// TODO Auto-generated method stub
 			System.out.println("start");
 			
+			String a1="123";
+			String a2="0123";
+			String a3=a2.substring(1, 4);
+						
+			System.out.println(a1==a3);
+			System.out.println(a1.equals(a3));
 			
 			ConcreteListener l=new ConcreteListener();
 			AsyncConcreteListener al=new AsyncConcreteListener(); 
